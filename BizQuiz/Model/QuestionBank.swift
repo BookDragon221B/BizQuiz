3811b0cd7cd1f6f028a2262e9436db6063902167
Taryn Neal <tarynneal@taryns-imac.fios-router.home>
2018-11-20 18:31:52 -0800

    Put in classes for question banks by changing the file BasicParliQuestionBank to QuestionBank and adding classes for the other topics question banks.

---
+++ BizQuiz/Model/QuestionBank.swift
@@ -0,0 +1,90 @@
+//
+//  BasicParliQuestionBank.swift
+//  BizQuiz
+//
+//  Created by Taryn Neal on 11/18/18.
+//  Copyright © 2018 Taryn Neal. All rights reserved.
+//
+
+import Foundation
+
+class BasicParliQuestionBank {
+    
+    var list = [Question]()
+    
+    init() {
+    
+        // Creating the quiz item inside the append function
+        list.append(Question(text: "Which of these is one one of the specific goals of FBLA stated in Article II of the FBLA Bylaws? ", correctAnswer: "A"))
+        
+        list.append(Question(text: "Article IV of the FBLA bylaws covers the organization of FBLA.", correctAnswer: "D"))
+       
+    }
+    
+}
+
+//Question Bank for the topic business skills.
+class BusinessSkillsQuestionBank {
+    
+    var list = [Question]()
+    
+    init() {
+        
+    }
+    
+}
+
+//Question Bank for the topic FBLA History
+class FBLAHistoryQuestionBank {
+    
+    var list = [Question]()
+    
+    init() {
+        
+    }
+    
+}
+
+//Question Bank for the topic National Conference Dates & Locations (NCD&L)
+class NCD&LQuestionBank {
+    
+    var list = [Question]()
+    
+    init() {
+     
+    }
+    
+}
+
+//Question Bank for the topic National Sponsers
+class NationalSponsersQuestionBank {
+    
+    var list = [Question]()
+    
+    init() {
+   
+    }
+    
+}
+
+//Question Bank for the topic competitive events
+class CompetitiveEventsQuestionBank {
+    
+    var list = [Question]()
+    
+    init() {
+     
+    }
+    
+}
+
+//Question Bank for the topic running for national office
+class RunningForNationalOfficeQuestionBank {
+    
+    var list = [Question]()
+    
+    init() {
+        
+    }
+    
+}
